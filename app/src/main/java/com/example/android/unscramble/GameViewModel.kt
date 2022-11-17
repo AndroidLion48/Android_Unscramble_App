4ecb2aa787a06d330fcc205f7802e808efa0e4e7
AndroidLion48 <>
2022-11-16 17:32:05 -0800

    Added ViewModel class and Delegated in GameFragment

---
+++ app/src/main/java/com/example/android/unscramble/GameViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.android.unscramble
+
+import androidx.lifecycle.ViewModel
+
+/**
+ * Created by Clarence E Moore on 2022-11-16.
+ *
+ * Description:
+ *
+ *
+ */
+class GameViewModel: ViewModel() {
+}
